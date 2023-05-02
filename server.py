9f26e1a1fad535cf4f75bd62ff7af5bb068167df
ElDokmak <a7medeldokmak@gmail.com>
2023-05-02 21:51:06 +0200

    Server-Client

--- server.py
+++ server.py
@@ -1,53 +1,44 @@
 import socket
 import threading
 
-PORT = 5050
-FORMAT = "utf-8"
-D = "DES"
 HEADER = 64
+PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
-# 172.22.128.1
 ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
 
-def send(msg):
-    MSG = msg.encode(FORMAT)
-    msg_len = len(MSG)
-    send_len = str(msg_len).encode(FORMAT)
-    send_len += b' '*(HEADER - len(send_len))
-    client.send(send_len)
-    client.send(MSG)
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
 
-
-def client(conn, addr, clients):
-    print(f"{addr} is connected")
     connected = True
     while connected:
-        msg_len = conn.recv(HEADER).decode(FORMAT)
-        if msg_len:
-            msg_len = int(msg_len)
-            msg = conn.recv(msg_len).decode(FORMAT)
-            if msg == D:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
                 connected = False
-            print(f"{addr} {msg}")
-        for client in clients:
-            if client != conn:
-                send(msg, client)
+
+            print(f"[{addr}] {msg}")
+            conn.send("Msg received".encode(FORMAT))
+
     conn.close()
 
 
 def start():
     server.listen()
-    print(f'The server is listening to {SERVER}')
-    clients = []
+    print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
         conn, addr = server.accept()
-        clients.append(conn)
-        thread = threading.Thread(target=client, args=(conn, addr, clients))
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
 
-print('The server is starting')
+print("[STARTING] server is starting...")
 start()
